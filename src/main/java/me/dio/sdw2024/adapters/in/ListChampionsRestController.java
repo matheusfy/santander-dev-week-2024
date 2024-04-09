2f75cf0b2bbfb8635c475ed53aa73e4f60ce4906
matheusfy <matheusy@alunos.utfpr.edu.br>
2024-04-09 19:26:20 -0300

    Configurate cors to permit conection [DIO]

--- src/main/java/me/dio/sdw2024/adapters/in/ListChampionsRestController.java
+++ src/main/java/me/dio/sdw2024/adapters/in/ListChampionsRestController.java
@@ -3,6 +3,7 @@ package me.dio.sdw2024.adapters.in;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import me.dio.sdw2024.application.ListChampionsUseCase;
 import me.dio.sdw2024.domain.model.ChampionRec;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 @RequestMapping("/champions")
 public record ListChampionsRestController(ListChampionsUseCase useCase) {
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500/")
     @GetMapping
     public List<ChampionRec> findAll(){
         return useCase.findAll();
